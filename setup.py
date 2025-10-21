e365f86d84371b641eef78d6d8f9a170ec05c4a5
Selahattin Nazli <selahattinnazli01@gmail.com>
2025-10-21 22:14:22 +0300

    Update setup.py with package configuration

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
 from setuptools import setup, find_packages
-from typing import List
-
-def get_requirements() -> List[str]:
-    requirement_list = []
-    try:
-        with open('requirements.txt', 'r') as f:
-            requirement_list = [req.strip() for req in f.readlines()]
-            if '-e .' in requirement_list:
-                requirement_list.remove('-e .')
-    except FileNotFoundError:
-        pass
-    return requirement_list
 
 setup(
     name="us_visa",
-    version="0.0.1",
+    version="0.0.0",
     author="selahattin",
-    author_email="selahattinnazli3@gmail.com",
+    author_email="selahattinnazli@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements(),
-    python_requires=">=3.8",
 )
