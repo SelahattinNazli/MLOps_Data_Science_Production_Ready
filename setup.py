78954f2391bc39233d68a7519e67ffa98754489d
Selahattin Nazli <selahattinnazli01@gmail.com>
2025-10-24 19:11:43 +0300

    Added eda.py file to notebook folder

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="selahattin",
-    author_email="selahattinnazli@gmail.com",
+    author="SelahattinNazli",
+    author_email="selahattinnazli01@gmail.com",
     packages=find_packages(),
 )
